02d2d6c9e4033d19ac891b3f034c883da641771b
Matthieu Falce <matthieu@falce.net>
2024-11-12 17:02:42 +0100

    gitautosync update 52

--- tape_en_cours/fizzbuzz.py
+++ tape_en_cours/fizzbuzz.py
@@ -9,13 +9,16 @@
 #         print(nombre)
 
 
-for nombre in range(1, 101):
-    match (nombre % 3, nombre % 5):
-        case (0, 0):
-            print("fizzbuzz")
-        case (0, _):
-            print("fizz")
-        case (_, 0):
-            print("buzz")
-        case (_, _):
-            print(nombre)
+# for nombre in range(1, 101):
+#     match (nombre % 3, nombre % 5):
+#         case (0, 0):
+#             print("fizzbuzz")
+#         case (0, _):
+#             print("fizz")
+#         case (_, 0):
+#             print("buzz")
+#         case (_, _):
+#             print(nombre)
+
+
+"fizzbuzz" = "fizz" + "buzz"
