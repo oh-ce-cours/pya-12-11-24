dab32fa8fe70e3284ac7cc51bb35c34358c1632c
Matthieu Falce <matthieu@falce.net>
2024-11-12 16:34:42 +0100

    gitautosync update 40

--- tape_en_cours/fizzbuzz.py
+++ tape_en_cours/fizzbuzz.py
@@ -10,11 +10,11 @@
 
 for nombre in range(1, 101):
     match (nombre % 3, nombre % 5):
-        case (0, 0):
-            print("fizzbuzz")
         case (0, _):
             print("fizz")
         case (_, 0):
             print("buzz")
         case (_, _):
             print(nombre)
+        case (0, 0):
+            print("fizzbuzz")
