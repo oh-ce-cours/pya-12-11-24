36ac7a4fd2549eb36a1649d603fc606c46ac71d9
Matthieu Falce <matthieu@falce.net>
2024-11-13 10:16:55 +0100

    gitautosync update 99

--- tape_en_cours/test_fizzbuzz.py
+++ tape_en_cours/test_fizzbuzz.py
@@ -1,1 +1,13 @@
 from fizzbuzz import traitement_fizz_buzz
+
+
+def test_multiple_de_trois():
+    # arange
+    nombre_entre = 3
+    attendu = "fizz"
+
+    # act
+    resultat = traitement_fizz_buzz(nombre_entre)
+
+    # assert
+    assert resultat == attendu
