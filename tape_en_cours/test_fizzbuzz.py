3bbdf50452be1b4bb4682c7dc5da5dcd510e89c6
Matthieu Falce <matthieu@falce.net>
2024-11-13 11:39:58 +0100

    gitautosync update 143

--- tape_en_cours/test_fizzbuzz.py
+++ tape_en_cours/test_fizzbuzz.py
@@ -2,7 +2,7 @@ from fizzbuzz import traitement_fizz_buzz
 import pytest
 
 
-@pytest.mark.parametrize("nombre_entre", [3, 99], "attendu", ["fizz", "fizz"])
+@pytest.mark.parametrize("nombre_entre, attendu", [(3, "fizz"), (99, "fizz")])
 def test_fizzbuzz_3_est_fizz(nombre_entre, attendu):
     # arange
 
