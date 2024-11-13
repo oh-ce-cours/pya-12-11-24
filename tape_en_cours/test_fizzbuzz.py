64c051b3bea64c43871f77a96f3902a9d4eafe47
Matthieu Falce <matthieu@falce.net>
2024-11-13 09:59:10 +0100

    gitautosync update 93

--- tape_en_cours/test_fizzbuzz.py
+++ tape_en_cours/test_fizzbuzz.py
@@ -1,13 +1,3 @@
 from fizzbuzz import traitement_fizz_buzz
 
 print("dans test", __name__)
-
-for nombre in range(10):
-    mon_nombre = nombre * 2
-
-if nombre == 8:
-    toto = 3
-
-print(nombre)
-print(mon_nombre)
-print(toto)
