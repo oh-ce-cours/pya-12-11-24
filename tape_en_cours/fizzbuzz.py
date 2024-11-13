cb8e09736d42ea7db2d7845d5e3386733e5ef8a8
Matthieu Falce <matthieu@falce.net>
2024-11-13 15:59:43 +0100

    gitautosync update 204

--- tape_en_cours/fizzbuzz.py
+++ tape_en_cours/fizzbuzz.py
@@ -53,7 +53,7 @@ def main():
     #     res = traitement_fizz_buzz(nombre)
     #     print(res)
 
-    print(map(traitement_fizz_buzz, range(1, 101)))
+    print(list(map(traitement_fizz_buzz, range(1, 101))))
 
 
 if __name__ == "__main__":
