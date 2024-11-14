e9e5f5ac34e61090a3f83a1c247b44c10b6a9b68
Matthieu Falce <matthieu@falce.net>
2024-11-14 19:11:58 +0100

    gitautosync update 161

--- tail_follow.py
+++ tail_follow.py
@@ -17,31 +17,6 @@ def tail_F(some_file):
             elif sleep_sec:
                 time.sleep(sleep_sec)
 
-    while True:
-        try:
-            with open(some_file, encoding="utf8") as input_file:
-                if first_call:
-                    input_file.seek(0, 2)
-                    first_call = False
-                latest_data = input_file.read()
-                while True:
-                    if "\n" not in latest_data:
-                        latest_data += input_file.read()
-                        if "\n" not in latest_data:
-                            yield ""
-                            if not os.path.isfile(some_file):
-                                break
-                            continue
-                    latest_lines = latest_data.split("\n")
-                    if latest_data[-1] != "\n":
-                        latest_data = latest_lines[-1]
-                    else:
-                        latest_data = input_file.read()
-                    for line in latest_lines[:-1]:
-                        yield line + "\n"
-        except IOError:
-            yield ""
-
 
 for line in tail_F("./log.txt"):
     if not line:
