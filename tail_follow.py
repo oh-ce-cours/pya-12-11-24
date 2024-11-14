ed4dbe1d78d7a6447864e79e4a6c56e48edfddcc
Matthieu Falce <matthieu@falce.net>
2024-11-14 19:06:53 +0100

    gitautosync update 152

--- tail_follow.py
+++ tail_follow.py
@@ -2,14 +2,14 @@ def tail_F(some_file):
     first_call = True
     while True:
         try:
-            with open(some_file) as input:
+            with open(some_file) as input_file:
                 if first_call:
-                    input.seek(0, 2)
+                    input_file.seek(0, 2)
                     first_call = False
-                latest_data = input.read()
+                latest_data = input_file.read()
                 while True:
                     if "\n" not in latest_data:
-                        latest_data += input.read()
+                        latest_data += input_file.read()
                         if "\n" not in latest_data:
                             yield ""
                             if not os.path.isfile(some_file):
@@ -19,7 +19,7 @@ def tail_F(some_file):
                     if latest_data[-1] != "\n":
                         latest_data = latest_lines[-1]
                     else:
-                        latest_data = input.read()
+                        latest_data = input_file.read()
                     for line in latest_lines[:-1]:
                         yield line + "\n"
         except IOError:
