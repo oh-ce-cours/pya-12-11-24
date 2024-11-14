7f42dc020ed47da2f5fb63eb964085129948ce8a
Matthieu Falce <matthieu@falce.net>
2024-11-14 12:03:36 +0100

    gitautosync update 53

---
+++ tape_en_cours/decorator.py
@@ -0,0 +1,19 @@
+def f(param):
+    print("dans f", param)
+    return param
+
+
+def deco(une_fonction):
+    def wrapper(param):
+        print("avant")
+        res = une_fonction(param)
+        print("apres")
+        return res
+
+    return wrapper
+
+
+print(f)
+f = deco(f)
+print(f)
+print(f("toto"))
