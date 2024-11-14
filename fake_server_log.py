1e93d4938406e7453f1e58fbf2324cf8785b101d
Matthieu Falce <matthieu@falce.net>
2024-11-14 19:19:53 +0100

    gitautosync update 176

--- fake_server_log.py
+++ fake_server_log.py
@@ -1,6 +1,7 @@
 import random 
 from pathlib import Path 
 
-with open(Path(".") / ".." / "supports" / "medias" / "fonctiondAvancees" / "logs_apache.txt")
+with open(Path(".") / ".." / "supports" / "medias" / "fonctiondAvancees" / "logs_apache.txt") as f:
+    lines = f.readlines()
 while True:
     
