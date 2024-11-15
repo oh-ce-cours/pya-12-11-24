d216b9129d958ed3adca86e127a290b5c5384cd6
Matthieu Falce <matthieu@falce.net>
2024-11-15 09:43:39 +0100

    gitautosync update 13

--- tape_en_cours/demo_multiprocessing.py
+++ tape_en_cours/demo_multiprocessing.py
@@ -12,6 +12,6 @@ def f(x):
 if __name__ == "__main__":
     tic = time.time()
     nb_processus_python_lances = 20  # proche du nombre de coeur physiques
-    with Pool(20) as p:
+    with Pool(nb_processus_python_lances) as p:
         print(p.map(f, range(600)))
     print(f"total time: {time.time()-tic}")
